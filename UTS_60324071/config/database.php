2cfb95599c985a007b9a72b11b966e73be07cf3f
Azril <moch.nazril.ilham24071@mhs.uingusdur.ac.id>
2026-05-04 20:16:09 +0700

    Delete UTS_60324071 directory

--- UTS_60324071/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "uts_perpustakaan_60324071");
-
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-}
-
-$conn->set_charset("utf8mb4");
-?>
